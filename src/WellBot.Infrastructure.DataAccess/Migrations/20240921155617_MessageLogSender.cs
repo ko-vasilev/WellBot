749c00ce14dcbcac56bb13591e190a7ceb6afb86
Konstantin Vasilev <oppa.kostya.bko@gmail.com>
2024-09-21 18:56:39 +0300

    Store information about who sent the message

---
+++ src/WellBot.Infrastructure.DataAccess/Migrations/20240921155617_MessageLogSender.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WellBot.Infrastructure.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class MessageLogSender : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Sender",
+                table: "MessageLogs",
+                type: "TEXT",
+                unicode: false,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sender",
+                table: "MessageLogs");
+        }
+    }
+}
