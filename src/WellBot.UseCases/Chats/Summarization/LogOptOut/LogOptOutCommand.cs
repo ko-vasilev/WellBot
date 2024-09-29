2d1d65bdbcfbe174da981a541a754d7d38ae13d3
Konstantin Vasilev <oppa.kostya.bko@gmail.com>
2024-09-29 16:07:14 +0300

    Add ability to opt out and opt in for messages logging

---
+++ src/WellBot.UseCases/Chats/Summarization/LogOptOut/LogOptOutCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+
+namespace WellBot.UseCases.Chats.Summarization.LogOptOut;
+
+/// <summary>
+/// Command for opting out from message logging.
+/// </summary>
+public class LogOptOutCommand : IRequest<Unit>, IChatInfo
+{
+    /// <summary>
+    /// Id of the telegram chat.
+    /// </summary>
+    public required long ChatId { get; init; }
+
+    /// <summary>
+    /// Id of the user who has sent the request.
+    /// </summary>
+    public long TelegramUserId { get; init; }
+
+    /// <summary>
+    /// Id of the message that triggered the command.
+    /// </summary>
+    public int MessageId { get; init; }
+}
